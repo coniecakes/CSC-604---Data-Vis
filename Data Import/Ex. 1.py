2278e2b788752eb9dbcbc6fd6d3e55133d6e7438
Conie O'Malley <co1984a@american.edu>
2024-06-18 19:03:21 -0400

    6/18 Class Commit

---
+++ Data Import/Ex. 1.py
@@ -0,0 +1,10 @@
+# Import the stockvalues.csv file.
+# The data is for dates, high, open and low values for a stock.
+# Use the dates list for X-axes and use the high, open and low for y axes.
+# Then draw a 3x2 grid of charts and display the lines for high, open and low in three of the boxes in the 3x2 grid.
+
+import matplotlib.pyplot as plt
+from matplotlib import style
+import csv
+
+stock_high
